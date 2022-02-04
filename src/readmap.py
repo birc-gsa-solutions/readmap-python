d5bdcb7dcdebf8dd58cb67a9f4ac4e4da0e4a195
Thomas Mailund <mailund@birc.au.dk>
2022-02-04 14:18:26 +0100

    adapted to Python

---
+++ src/readmap.py
@@ -0,0 +1,42 @@
+import argparse
+import sys
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description="Readmapper",
+        usage="\n\treadmap -p genome\n\treadmap -d dist genome reads"
+    )
+    argparser.add_argument(
+        "-p", action="store_true",
+        help="preprocess the genome."
+    )
+    argparser.add_argument(
+        "-d", type=int, metavar="integer",
+        default=1, help="max edit distance."
+    )
+    argparser.add_argument(
+        "genome",
+        help="Simple-FASTA file containing the genome.",
+        type=argparse.FileType('r')
+    )
+    argparser.add_argument(
+        "reads", nargs="?",
+        help="Simple-FASTQ file containing the reads.",
+        type=argparse.FileType('r')
+    )
+    args = argparser.parse_args()
+
+    if args.p:
+        print(f"Preprocess {args.genome}")
+    else:
+        # here we need the optional argument reads
+        if args.reads is None:
+            argparser.print_help()
+            sys.exit(1)
+        print(
+            f"Search {args.genome} for {args.reads} within distance {args.d}")
+
+
+if __name__ == '__main__':
+    main()
