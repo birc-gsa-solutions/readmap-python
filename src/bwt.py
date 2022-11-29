21a4f685b90e4a8a6b00e96e6bf2edb3d1ccb46b
Thomas Mailund <mailund@birc.au.dk>
2022-11-29 13:48:50 +0100

    returned to co-routines

--- src/bwt.py
+++ src/bwt.py
@@ -1,8 +1,8 @@
 """Implementatin of the Burrows-Wheeler transform and related algorithms."""
 
-import enum
 from typing import (
-    Iterator, Callable,
+    Iterator,
+    Callable,
     NamedTuple
 )
 import numpy as np
@@ -88,16 +88,22 @@ class FMIndexTables(NamedTuple):
     ctab: npt.NDArray[np.int32]
     otab: npt.NDArray[np.int32]
     rotab: npt.NDArray[np.int32]
+    dtab: list[int]
+    edit_ops: list[Edit]
+    p: bytearray
 
 
-def preprocess_tables(x: str) -> FMIndexTables:
+def preprocess_tables(x: str) -> \
+    tuple[Alphabet, array,
+          npt.NDArray[np.int32],
+          npt.NDArray[np.int32], npt.NDArray[np.int32]]:
     """Preprocess tables for exact FM/bwt search."""
     bwt, alpha, sa = burrows_wheeler_transform(x)
     ctab = build_ctab(bwt, len(alpha))
     otab = build_otab(bwt, len(alpha))
     bwt, alpha, _ = burrows_wheeler_transform(x[::-1])
     rotab = build_otab(bwt, len(alpha))
-    return FMIndexTables(alpha, sa, ctab, otab, rotab)
+    return alpha, sa, ctab, otab, rotab
 
 
 def build_dtab(
@@ -120,28 +126,66 @@ def build_dtab(
     return dtab
 
 
-# State machine
-class MatchState(enum.Enum):
-    """Edit operations."""
+def do_m(tbls: FMIndexTables,
+         i: int, left: int, right: int,
+         edits: int) -> Iterator[tuple[int, str]]:
+    """Perform a match/mismatch operation in the approx search."""
+    tbls.edit_ops.append(Edit.MATCH)
+    for a in range(1, len(tbls.alpha)):
+        next_left = tbls.ctab[a] + tbls.otab[a, left]
+        next_right = tbls.ctab[a] + tbls.otab[a, right]
+        if next_left >= next_right:
+            continue
 
-    DONE = enum.auto()
-    REC = enum.auto()
-    MATCH = enum.auto()
-    INSERT = enum.auto()
-    DELETE = enum.auto()
-    POP_NEXT = enum.auto()
+        next_edits = edits - (a != tbls.p[i])
+        yield from rec_search(tbls, i - 1, next_left, next_right, next_edits)
+    tbls.edit_ops.pop()
 
 
-class MatchFrame(NamedTuple):
-    """Stack frame when storing processing states for later."""
+def do_i(tbls: FMIndexTables,
+         i: int, left: int, right: int,
+         edits: int) -> Iterator[tuple[int, str]]:
+    """Perform an insertion operation in the approx search."""
+    tbls.edit_ops.append(Edit.INSERT)
+    yield from rec_search(tbls, i - 1, left, right, edits - 1)
+    tbls.edit_ops.pop()
 
-    state: MatchState  # The kind of state we have
-    left: int          # left in search interval
-    right: int         # right in search interval
-    pos: int           # position into pattern
-    a: int             # character
-    edits_idx: int     # index into the edits stack
-    max_edits: int     # edits we have left
+
+def do_d(tbls: FMIndexTables,
+         i: int, left: int, right: int,
+         edits: int) -> Iterator[tuple[int, str]]:
+    """Perform a deletion operation in the approx search."""
+    tbls.edit_ops.append(Edit.DELETE)
+    for a in range(1, len(tbls.alpha)):
+        next_left = tbls.ctab[a] + tbls.otab[a, left]
+        next_right = tbls.ctab[a] + tbls.otab[a, right]
+        if next_left >= next_right:
+            continue
+        yield from rec_search(tbls, i, next_left, next_right, edits - 1)
+    tbls.edit_ops.pop()
+
+
+def rec_search(tbls: FMIndexTables,
+               i: int, left: int, right: int,
+               edits: int) \
+        -> Iterator[tuple[int, str]]:
+    """Handle recursive operations in approx search."""
+    # Do we have a match here?
+    if i < 0 <= edits:
+        # Remember to reverse the operations, since
+        # we did the backwards in the bwt search
+        cigar = edits_to_cigar(tbls.edit_ops[::-1])
+        for j in range(left, right):
+            yield tbls.sa[j], cigar
+        return
+
+    # Can we get to a match with the edits we have left?
+    if edits < tbls.dtab[i]:
+        return
+
+    yield from do_m(tbls, i, left, right, edits)
+    yield from do_i(tbls, i, left, right, edits)
+    yield from do_d(tbls, i, left, right, edits)
 
 
 def approx_searcher_from_tables(
@@ -153,7 +197,7 @@ def approx_searcher_from_tables(
 ) -> ApproxSearchFunc:
     """Build an exact search function from preprocessed tables."""
     # @profile
-    def search(p_: str, max_edits: int) -> Iterator[tuple[int, str]]:
+    def search(p_: str, edits: int) -> Iterator[tuple[int, str]]:
         assert p_, "We can't do approx search with an empty pattern!"
         try:
             p = alpha.map(p_)
@@ -161,104 +205,15 @@ def approx_searcher_from_tables(
             return  # can't map, so no matches
 
         dtab = build_dtab(p, sa, ctab, rotab)
-        left, right, pos = 0, len(sa), len(p) - 1
-        a, edits_idx = -1, 0
-        edits: list[Edit] = [Edit.MATCH] * (len(p) + max_edits)
-        stack: list[MatchFrame] = [
-            MatchFrame(MatchState.DONE, left, right, pos, -1, -1, -1)
-        ]
+        tbls = FMIndexTables(alpha, sa,
+                             ctab, otab, rotab, dtab,
+                             list[Edit](), p)
 
-        state = MatchState.REC
-        while True:
-            match state:
-                case MatchState.DONE:
-                    # If we get to the bottom of the stack and pop a 'DONE'
-                    # state we are done searching.
-                    return
-
-                case MatchState.REC:
-                    if left == right or max_edits < dtab[pos]:
-                        state = MatchState.POP_NEXT
-                        continue
-
-                    if pos < 0:
-                        # We have a hit!
-                        cigar = edits_to_cigar(edits[:edits_idx][::-1])
-                        for j in range(left, right):
-                            yield sa[j], cigar
-                        state = MatchState.POP_NEXT  # go to the next state
-                        continue
-
-                    # If we haven't reached the end or have something to report
-                    # we continue with a match...
-                    a = 1  # start after the sentinel
-                    state = MatchState.MATCH
-                    continue
-
-                case MatchState.MATCH:
-                    if a == len(alpha):
-                        # done with matching, so move on to insertion
-                        state = MatchState.INSERT
-                        continue
-
-                    # remember to do the next match later
-                    stack.append(MatchFrame(
-                        MatchState.MATCH,
-                        left, right, pos, a+1,
-                        edits_idx, max_edits
-                    ))
-                    # but do a recursion here...
-                    state = MatchState.REC
-                    left = int(ctab[a]) + int(otab[a, left])
-                    right = int(ctab[a]) + int(otab[a, right])
-                    max_edits -= int(a != p[pos])
-                    pos -= 1
-                    edits[edits_idx] = Edit.MATCH
-                    edits_idx += 1
-                    continue
-
-                case MatchState.INSERT:
-                    # remember to do a deletion later
-                    stack.append(MatchFrame(
-                        MatchState.DELETE,
-                        left, right, pos, 1,
-                        edits_idx, max_edits
-                    ))
-                    # but for now, continue with a rec
-                    state = MatchState.REC
-                    pos -= 1
-                    max_edits -= 1
-                    edits[edits_idx] = Edit.INSERT
-                    edits_idx += 1
-                    continue
-
-                case MatchState.DELETE:
-                    if a == len(alpha) or edits_idx == 0:
-                        # we are either done with deletions or shouldn't
-                        # start because we are at the beginning of the
-                        # recursion
-                        state = MatchState.POP_NEXT
-                        continue
-
-                    # Push another deletion for later
-                    stack.append(MatchFrame(
-                        MatchState.DELETE,
-                        left, right, pos, a + 1,
-                        edits_idx, max_edits
-                    ))
-                    # and do a recursion for now
-                    state = MatchState.REC
-                    left = int(ctab[a]) + int(otab[a, left])
-                    right = int(ctab[a]) + int(otab[a, right])
-                    max_edits -= 1
-                    edits[edits_idx] = Edit.DELETE
-                    edits_idx += 1
-                    continue
-
-                case MatchState.POP_NEXT:
-                    state, left, right, pos, a, edits_idx, max_edits \
-                        = stack.pop()
-                    continue
+        # Do the first operation in this function to avoid
+        # deletions in the beginning (end) of the search
+        left, right, i = 0, len(sa), len(p) - 1
+        yield from do_m(tbls, i, left, right, edits)
+        yield from do_i(tbls, i, left, right, edits)
 
     return search
 
